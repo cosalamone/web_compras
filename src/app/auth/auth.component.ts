33ed415ee0cd2dcf4e77601d9bc420066d50b5a4
cosalamone <cosalamone@gmail.com>
2023-06-14 23:00:11 -0300

    form para registrar usuario

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Tenes que ingresar un e-mail';
+    }
+
+    return this.email.hasError('email') ? 'E-mail invalido' : '';
+  }
+
+
+  cerrarDialog(){}
+
+  crearUsuario(){}
 }
